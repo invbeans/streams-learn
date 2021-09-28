4ea4c56033debc4042887bcae0c704d4bf552a06
rassafel <rassafel@gmail.com>
2021-09-28 10:34:22 +0400

    Created method for work with primitive streams.

--- src/main/java/ru/rassafel/streams/lecture/Application.java
+++ src/main/java/ru/rassafel/streams/lecture/Application.java
@@ -10,4 +10,29 @@ public class Application {
     public static void main(String[] args) {
         Application application = new Application();
     }
+
+    public void primitiveStreams() {
+        List<Integer> integerList = new Random()
+            .ints(10, 100)
+            .peek(x -> System.out.println("Input value: " + x))
+            .filter(x -> x % 2 != 0)
+            .peek(x -> System.out.println("Filtered value: " + x))
+            .limit(10)
+            .map(x -> x * 2)
+            .peek(x -> System.out.println("Output value: " + x))
+            .boxed()
+            .collect(Collectors.toList());
+        System.out.println();
+        System.out.println("integerList = " + integerList);
+
+        List<Integer> mappedIntegerList = integerList.stream()
+            .filter(x -> x % 5 == 0)
+            .collect(Collectors.toList());
+
+        System.out.println();
+        System.out.println("integerList = " + integerList);
+        System.out.println("mappedIntegerList = " + mappedIntegerList);
+        System.out.println(integerList.equals(mappedIntegerList));
+        System.out.println("integerList.getClass() = " + integerList.getClass());
+    }
 }
