24fe8aa5c800bb76126f931971de4a2e1da220a1
rassafel <rassafel@gmail.com>
2021-09-28 10:33:50 +0400

    Created stream lecture main class.

---
+++ src/main/java/ru/rassafel/streams/lecture/Application.java
@@ -0,0 +1,13 @@
+package ru.rassafel.streams.lecture;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * @author rassafel
+ */
+public class Application {
+    public static void main(String[] args) {
+        Application application = new Application();
+    }
+}
